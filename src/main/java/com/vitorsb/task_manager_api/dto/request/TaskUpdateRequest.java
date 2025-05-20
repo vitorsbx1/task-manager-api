c93ea3dfaea94587cb8dd10dc1465310c42aa76c
sbx <vsbreturn@gmail.com>
2025-05-20 18:58:47 -0300

    Adicionado novos testes para classe de TaskService

--- src/main/java/com/vitorsb/task_manager_api/dto/request/TaskUpdateRequest.java
+++ src/main/java/com/vitorsb/task_manager_api/dto/request/TaskUpdateRequest.java
@@ -2,6 +2,8 @@ package com.vitorsb.task_manager_api.dto.request;
 
 import com.vitorsb.task_manager_api.model.enums.TaskPriority;
 import com.vitorsb.task_manager_api.model.enums.TaskStatus;
+import lombok.Builder;
 
+@Builder
 public record TaskUpdateRequest(String description, TaskPriority priority, TaskStatus status) {
 }
--- src/test/java/com/vitorsb/task_manager_api/service/impl/TaskServiceImplTest.java
+++ src/test/java/com/vitorsb/task_manager_api/service/impl/TaskServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.vitorsb.task_manager_api.service.impl;
 
+import com.vitorsb.task_manager_api.converter.TaskConverter;
 import com.vitorsb.task_manager_api.dto.request.TaskRequest;
+import com.vitorsb.task_manager_api.dto.request.TaskUpdateRequest;
 import com.vitorsb.task_manager_api.dto.response.TaskResponse;
 import com.vitorsb.task_manager_api.model.Task;
 import com.vitorsb.task_manager_api.model.enums.TaskPriority;
@@ -12,14 +14,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.verify;
@@ -126,6 +130,96 @@ class TaskServiceImplTest {
         }
 
         verify(taskRepository).findTask(any(), any(), isNull(), isNull());
+    }
+
+    @Test
+    void shoudFindTaskSucessfully(){
+        Long id = 1L;
+
+        Task task = Task
+                .builder()
+                .id(1L)
+                .priority(TaskPriority.LOW)
+                .build();
+
+        when(taskRepository.findById(any())).thenReturn(Optional.ofNullable(task));
+
+        TaskResponse response = taskService.getTask(id);
+
+        assertThat(response.id()).isNotNull();
+
+        verify(taskRepository).findById(any());
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenTaskNotFound(){
+        Long id = 1L;
+
+        when(taskRepository.findById(any())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> taskService.getTask(id))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining("Task not found");
+    }
+
+
+    @Test
+    void shouldUpdateTaskSucessfully(){
+        Long id = 1L;
+
+        TaskUpdateRequest taskUpdateRequest = TaskUpdateRequest.builder()
+                .description("TESTE1")
+                .status(TaskStatus.COMPLETED)
+                .priority(TaskPriority.LOW)
+                .build();
+
+        Task task = Task
+                .builder()
+                .id(1L)
+                .description("TESTE")
+                .priority(TaskPriority.LOW)
+                .build();
+
+
+        when(taskRepository.findById(id)).thenReturn(Optional.of(task));
+        when(taskRepository.save(any(Task.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        TaskResponse response = taskService.updateTask(id, taskUpdateRequest);
+
+        assertThat(response.id()).isEqualTo(id);
+        assertThat(task.getDescription()).isEqualTo("TESTE1");
+        assertThat(task.getPriority()).isEqualTo(TaskPriority.LOW);
+        assertThat(task.getStatus()).isEqualTo(TaskStatus.COMPLETED);
+        assertThat(task.getCompletedAt()).isNotNull();
+
+        verify(taskRepository).findById(id);
+        verify(taskRepository).save(task);
+
+
+    }
+
+    @Test
+    void shouldUpdateDateExcludeSucessufully(){
+        Long id = 1L;
+
+
+        Task task = Task
+                .builder()
+                .id(1L)
+                .description("TESTE")
+                .priority(TaskPriority.LOW)
+                .build();
+
+
+        when(taskRepository.findById(id)).thenReturn(Optional.of(task));
+        when(taskRepository.save(any(Task.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        taskService.deleteTask(id);
+
+        assertThat(task.getExcludedAt()).isNotNull();
+        verify(taskRepository).findById(id);
+        verify(taskRepository).save(task);
     }
 
 
